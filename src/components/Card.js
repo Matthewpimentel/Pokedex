4bc35ec4df4342af2947054b6ece58a7ec746f35
matthewpimentel <matthewpimentel@hotmail.com>
2021-10-20 12:11:17 -0400

    added starting project

---
+++ src/components/Card.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import typeColors from '../helpers/typeColors'
+import '../App.css';
+
+function Card({ pokemon, PokemonMove }) {
+    return (
+        <div className="Card">
+            <div className="Card__img">
+                <img src={pokemon.sprites.front_default} alt="" />
+            </div>
+            <div className="Card__name">
+                {pokemon.name}
+            </div>
+            <div className="Card__types">
+                {
+                    pokemon.types.map(type => {
+                        return (
+                            <div className="Card__type" style={{ backgroundColor: typeColors[type.type.name] }}>
+                                {type.type.name}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="Card__info">
+                <div className="Card__data Card__data--weight">
+                    <p className="title">Weight</p>
+                    <p>{pokemon.weight}</p>
+                </div>
+                <div className="Card__data Card__data--weight">
+                    <p className="title">Height</p>
+                    <p>{pokemon.height}</p>
+                </div>
+                <div className="Card__data Card__data--ability">
+                    <p className="title">Ability</p>
+                    <p>{pokemon.abilities[0].ability.name}</p>
+                </div>
+                <div className="Card__data Card__data--stats">
+                    <p className="title">Base Experience</p>
+                    <p>{pokemon.base_experience}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
